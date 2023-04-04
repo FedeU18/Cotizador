a4f7e2b99d15976e5eb6b035c6c28cf2f6cda694
Federico <fedee.unates.2001@gmail.com>
2023-04-04 14:55:13 -0300

    aprendiendo sobre styled components

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,36 @@
+import styled from "@emotion/styled";
+import Cripto from "./assets/imagen-criptos.png";
+
+const Heading = styled.h1`
+  font-family: "Lato", sans-serif;
+  color: #fff;
+`;
+
+const Contenedor = styled.div`
+  max-width: 900px;
+  margin: 0 auto;
+  width: 90%;
+  @media (min-width: 992px) {
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    column-gap: 2rem;
+  }
+`;
+
+const Imagen = styled.img`
+  max-width: 400px;
+  width: 80%;
+  margin: 100px auto 0 auto;
+  display: block;
+`;
+
 function App() {
-  return <div></div>;
+  return (
+    <Contenedor>
+      <Imagen src={Cripto} alt="Imagen-criptomonedas" />
+      <Heading>Desde App</Heading>;
+    </Contenedor>
+  );
 }
 
 export default App;
